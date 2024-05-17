dde17c4233fbbc8bc32537449182b3e3a561f8bf
dev <dev@gmail.com>
2024-05-18 03:19:07 +0800

    export

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
         './components/**/*.{js,ts,jsx,tsx,mdx}',
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'main-color': '#58aa47',
+            },
+        },
     },
-    plugins: [],
+    plugins: [require('@tailwindcss/aspect-ratio')],
 };
