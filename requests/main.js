79f6edc59960a8a5e5d2745223a053d04378af71
dev <dev@gmail.com>
2024-05-18 03:27:39 +0800

    host

--- requests/main.js
+++ requests/main.js
@@ -1,9 +1,7 @@
 import request from '@/utils/fetcher';
 
-const API_HOST = 'http://localhost:3000';
-// const apiHost = typeof window !== 'undefined' ? _config.API_HOST : API_HOST;
-const apiHost = process.env.NEXT_PUBLIC_API_HOST || API_HOST;
+const host = process.env.NEXT_PUBLIC_API_HOST;
 
 export function getUsers(params) {
-    return request(`${apiHost}/users`, params);
+    return request(`${host}/users`, params);
 }
