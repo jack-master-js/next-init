87d94aa9382ad9b1d2f0f135ae774c44bec87b36
dev <dev@gmail.com>
2024-06-01 12:35:12 +0800

    theme-color

--- pages/_app.js
+++ pages/_app.js
@@ -16,6 +16,7 @@ export default function App({ Component, pageProps }) {
                     name="viewport"
                     content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
                 />
+                <meta name="theme-color" content="#000000" />
 
                 <link rel="manifest" href="/manifest.json" />
                 <link rel="icon" href="/favicon.ico" />
