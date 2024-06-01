745017854f231b49ccca139add95618026800546
dev <dev@gmail.com>
2024-06-01 12:12:02 +0800

    icons

--- pages/_app.js
+++ pages/_app.js
@@ -22,18 +22,14 @@ export default function App({ Component, pageProps }) {
                     content="default"
                 />
                 <meta name="apple-mobile-web-app-title" content="PWA App" />
-                <meta name="description" content="Best PWA App in the world" />
                 <meta name="format-detection" content="telephone=no" />
                 <meta name="mobile-web-app-capable" content="yes" />
-                <meta
-                    name="msapplication-config"
-                    content="/icons/browserconfig.xml"
-                />
+
                 <meta name="msapplication-TileColor" content="#2B5797" />
                 <meta name="msapplication-tap-highlight" content="no" />
                 <meta name="theme-color" content="#000000" />
 
-                <link
+                {/* <link
                     rel="apple-touch-icon"
                     href="/icons/touch-icon-iphone.png"
                 />
@@ -64,13 +60,8 @@ export default function App({ Component, pageProps }) {
                     type="image/png"
                     sizes="16x16"
                     href="/icons/favicon-16x16.png"
-                />
+                /> */}
                 <link rel="manifest" href="/manifest.json" />
-                <link
-                    rel="mask-icon"
-                    href="/icons/safari-pinned-tab.svg"
-                    color="#5bbad5"
-                />
                 <link rel="shortcut icon" href="/favicon.ico" />
             </Head>
             <StateProvider>
